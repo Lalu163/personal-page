8280a6c34d0deb07ef1cf02e8f0f880cad8e86f0
Laura Contreras <m.laura.cp89@gmail.com>
2022-05-18 19:29:57 +0200

    creating navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <div className="navbar">
+
+            <ul>
+                <li>Home</li>
+                <li>Portfolio</li>
+                <li>Contact</li>
+            </ul>
+
+        </div>
+    )
+}
+
+export default Navbar;
