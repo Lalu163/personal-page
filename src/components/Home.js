15ecdb2317af9d19002a7a1b58ed88fb891f8f29
Laura Contreras <m.laura.cp89@gmail.com>
2022-05-18 22:56:53 +0200

    adding a font

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home(){
+    return(
+        <div className="about-me">
+            <h1>About me</h1>
+            <p>After more than 6 years dedicated to the food and customer service sector, I decided to enter the
+                technology sector specializing in full stack web development with Java, SpringBoot and React. At times I am actively looking for these first positions as Junior Developer
+                and I continue programming in my own projects. Do not hesitate to contact me for more information and to determine how I can help your company to continue growing.</p>
+        </div>
+    );
+}
+
+export default Home;
