7bee835aa5569885ed86905eb11325a37f2091b7
Laura Contreras <m.laura.cp89@gmail.com>
2022-05-22 13:50:52 +0200

    grmmar in home

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ function Home() {
                     <p className="home-bio">After more than 6 years dedicated to the food and customer service sector, I
                         decided to enter the
                         technology sector specializing in full stack web development with Java, SpringBoot and React. At
-                        times I am actively looking for these first positions as Junior Developer
+                        the moment I am actively looking for these first positions as Junior Developer
                         and I continue programming in my own projects. Do not hesitate to contact me for more
                         information
                         and to determine how I can help your company to continue growing.</p>
